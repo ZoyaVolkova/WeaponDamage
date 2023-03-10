57d66c51e49d264a9fb953e04e951b97505049b3
ZoyaVolkova <zoearkhipova@gmail.com>
2023-03-10 11:56:44 +0300

    Добавьте файлы проекта.

---
+++ WeaponDamage/Class3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponDamage
+{
+    internal class WeaponDamage
+    {
+        private int roll;
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        
+        public int Damage { get; protected set; }
+
+        public WeaponDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+        protected virtual void CalculateDamage()
+        {
+            /*Переопределяется субклассом*/
+        }
+
+
+        private bool magic;
+
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        private bool flaming;
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+    }
+}
